fc126c993c3b13c1543851a38b4c54c21fbc1396
Jakub Smolaga <smolagakuba@gmail.com>
2025-04-25 21:37:28 +0200

    feat: add basic tests for strings module

---
+++ test_str.c
@@ -0,0 +1,32 @@
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "str.h"
+
+int
+main(void)
+{
+        String str = str_new();
+        str = str_push(str, 'H');
+        str = str_push(str, 'e');
+        str = str_push(str, 'l');
+        str = str_push(str, 'l');
+        str = str_push(str, 'o');
+        str = str_push(str, ' ');
+        str = str_push(str, 'W');
+        str = str_push(str, 'o');
+        str = str_push(str, 'r');
+        str = str_push(str, 'l');
+        str = str_push(str, 'd');
+        str = str_push(str, '!');
+        assert(strcmp(str, "Hello World!") == 0);
+        str = str_insert(str, 5, ',');
+        assert(strcmp(str, "Hello, World!") == 0);
+        str = str_insert(str, 0, 'H');
+        assert(strcmp(str, "HHello, World!") == 0);
+        str = str_cut(str, 6, 13);
+        printf("%s\n", str);
+        assert(strcmp(str, "HHello!") == 0);
+        str_free(str);
+        return 0;
+}
