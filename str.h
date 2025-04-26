b173fc18798639f129a5dd50c3a88dead6edf5eb
Jakub Smolaga <smolagakuba@gmail.com>
2025-04-26 22:53:20 +0200

    feat(str): add str_insert_str

--- str.h
+++ str.h
@@ -8,6 +8,7 @@ typedef char* String;
 static String str_new(void);
 static String str_push(String str, char c);
 static String str_insert(String str, int index, char c);
+static String str_insert_str(String str, int index, const char *insert);
 static String str_cut(String str, int beg, int end);
 static void str_free(String str);
 
@@ -77,6 +78,27 @@ str_insert(String str, int index, char c)
         hdr->len += 1;
         str[hdr->len] = '\0';
         return str;
+}
+
+static String
+str_insert_str(String str, int index, const char *insert)
+{
+	_StrHdr* hdr = _str_hdr(str);
+	int insert_len = 0;
+	while (insert[insert_len] != '\0') {
+		++insert_len;
+	}
+	hdr = _str_ensure(hdr, hdr->len + insert_len + 1);
+	str = (String)(&hdr[1]);
+	for (int i = hdr->len; i >= index; --i) {
+		str[i + insert_len] = str[i];
+	}
+	for (int i = 0; i < insert_len; ++i) {
+		str[index + i] = insert[i];
+	}
+	hdr->len += insert_len;
+	str[hdr->len] = '\0';
+	return str;
 }
 
 static String
